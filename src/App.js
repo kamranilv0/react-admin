3e36a44074788a6a98a93e2e2842b0d022fc57d6
kamranilv0 <kamranilv0@gmail.com>
2023-03-01 21:12:50 +0300

    dark mode first implementation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import Topbar from "./scenes/global/Topbar"
+
 function App() {
-  return (
-    <div className="app">
-      <h1>Hello</h1>
-    </div>
+  const [theme, colorMode] = useMode();
+
+  return (<ColorModeContext.Provider value={colorMode}>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div className="app">
+        <main className="context">
+          <Topbar />
+        </main>
+      </div>
+    </ThemeProvider>
+  </ColorModeContext.Provider>
   );
 }
 
